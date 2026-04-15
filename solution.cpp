1d94e28a26b077230c1c47f30615d7678059550d
LazyBrain-spiral <abhishek.study1018@gmail.com>
2026-04-15 23:46:53 +0530

    part1 sort

--- solution.cpp
+++ solution.cpp
@@ -1,99 +1,111 @@
-
-//fibonachi
-#include <bits/stdc++.h>
-using namespace std;
-
-class Solution
-{
-public:
-    int fib(int n)
-    {
-        if (n == 0 || n == 1)
-        {
-            return n;
-        }
-
-       return fib(n - 1) + fib(n - 2);
-    }
-};
-
-int
-main()
-    {
-        int n;
-        cin >> n;
-
-        int fib(n);
-    }
-
-
-
-//Hashing:
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-int main(){
-    int n;
-    cin >> n; //original array to get input from
-    
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    int hash[12] = {0};
-    for (int i = 0; i < n; i++)
-    {
-        hash[arr[i]] = hash[arr[i]]+1;
-    }
-    //hashing gets added here.basically for precomputing
-
-    int q;
-    cin >> q;
-    while(q--){
-        int number;
-        cin >> number;
-        cout << hash[number] << endl;
-    }
-
-    //this is the output on how many times it has done it already
-
-    
-
-    
-}
 #include <iostream>
-using namespace std;
+#include <vector>
+#include <algorithm> 
 
+
+void printArray(const std::vector<int> &arr)
+{
+    for (int val : arr)
+    {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+void selectionSort(std::vector<int> &arr)
+{
+    int n = arr.size();
+    // Traverse through all array elements
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Find the minimum element in unsorted array
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+            {
+                min_idx = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        std::swap(arr[i], arr[min_idx]);
+    }
+}
+
+// 2. Bubble Sort
+void bubbleSort(std::vector<int> &arr)
+{
+    int n = arr.size();
+    // Traverse through all array elements
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+
+        // Last i elements are already in place
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            // Swap if the element found is greater than the next element
+            if (arr[j] > arr[j + 1])
+            {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        // If no two elements were swapped by inner loop, then break
+        if (!swapped)
+        {
+            break;
+        }
+    }
+}
+
+// 3. Insertion Sort
+void insertionSort(std::vector<int> &arr)
+{
+    int n = arr.size();
+    // Traverse from 1 to n
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        // Move elements of arr[0..i-1] that are greater than key
+        // to one position ahead of their current position
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        // Place the key at its correct position
+        arr[j + 1] = key;
+    }
+}
+
+// Main function to test everything
 int main()
 {
-    string s;
-    cin >> s;
+    // Initialize three identical vectors for testing
+    std::vector<int> test_arr1 = {64, 25, 12, 22, 11};
+    std::vector<int> test_arr2 = {64, 25, 12, 22, 11};
+    std::vector<int> test_arr3 = {64, 25, 12, 22, 11};
 
-    // precompute
-    int hash[26] = {0};
-    for (int i = 0; i < s.size(); i++)
-    {
-        hash[s[i] - 'a']++;
-    }
+    std::cout << "Original array: ";
+    printArray(test_arr1);
+    std::cout << "-----------------------" << std::endl;
 
-    int q;
-    cin >> q;
+    // Test Selection Sort
+    selectionSort(test_arr1);
+    std::cout << "Selection Sort: ";
+    printArray(test_arr1);
 
-    while (q--)
-    {
-        char c;
-        cin >> c;
-
-        // fetch
-        cout << hash[c - 'a'] << endl;
-    }
+    // Test Bubble Sort
+    bubbleSort(test_arr2);
+    std::cout << "Bubble Sort:    ";
+    printArray(test_arr2);
+    insertionSort(test_arr3);
+    std::cout << "Insertion Sort: ";
+    printArray(test_arr3);
 
     return 0;
 }
-
-
-//sorting start(mostly was working on a personal project today)
